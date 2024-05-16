8df56c2d29e5d3da3750334327f388bb4b0a3b72
Bijoy Sarkar <162786532+ryukftw@users.noreply.github.com>
2024-05-16 09:11:06 +0000

    spartan: Use low gamma implementation for brightness curve

--- device.mk
+++ device.mk
@@ -154,6 +154,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.brightness.low.gamma=true
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
