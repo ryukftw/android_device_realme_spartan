384baef0398db163b4f20c5711830eb43c0716e6
ryukftw <workforbijoy9@gmail.com>
2024-08-21 22:22:40 +0000

    spartan: add gapps support
    https://gitlab.com/MindTheGapps/vendor_gapps.git

--- lineage_spartan.mk
+++ lineage_spartan.mk
@@ -20,6 +20,9 @@ PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3371
 
+# Gapps
+-include vendor/gapps/arm64/arm64-vendor.mk
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
