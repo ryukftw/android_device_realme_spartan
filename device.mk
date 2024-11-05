ce5bb93fb61b422f182231daec534d72167f4dcf
ryukftw <workforbijoy9@gmail.com>
2024-11-05 09:39:22 +0000

    spartan: Overlay Cleanup

--- device.mk
+++ device.mk
@@ -342,9 +342,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResTarget \
