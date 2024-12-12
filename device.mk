5f99092f18c973898b32a95f3cd37550efc1a63e
Bijoy Sarkar <workforbijoy9@gmail.com>
2024-12-12 11:37:11 +0000

    spartan: build the vendor version of libcrypto_shim
    Fix:
    F linker  : CANNOT LINK EXECUTABLE /odm/bin/hw/android.hardware.drm@1.3-service.widevine: library libcrypto_shim.so not found: needed by /odm/lib64/libwvhidl.so in namespace (default)
    
    Signed-off-by: ryukftw <workforbijoy9@gmail.com>

--- device.mk
+++ device.mk
@@ -178,7 +178,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim \
+    libcrypto_shim.vendor \
     libcrypto-v33 \
     wvmkiller
 
